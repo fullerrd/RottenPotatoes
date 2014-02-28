a329e284ee9e515b7ffe5613f8a3a95926c10d62
Dominic Fuller-Rowell <dominic.fuller-rowell@colorado.edu>
2014-02-27 18:10:20 -0700

    modified:   config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 
 module Rottenpotatoes
   class Application < Rails::Application
